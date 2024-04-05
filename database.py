4b0072a184e6921fcabc2c82b9c862ae0cc2faa1
Andrei Pernevan <apernevan@outlook.com>
2024-04-05 16:28:46 +0300

    course updates

--- database.py
+++ database.py
@@ -17,8 +17,27 @@ my_cursor = mydb.cursor()
 #     print(db[0])
 
 # Create a new table
-my_cursor.execute("CREATE TABLE IF NOT EXISTS users (name VARCHAR(255), email VARCHAR(255), age INTEGER(10), user_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
-my_cursor.execute("SHOW TABLES")
+# my_cursor.execute("CREATE TABLE IF NOT EXISTS users (name VARCHAR(255), email VARCHAR(255), age INTEGER(10), user_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
+# my_cursor.execute("SHOW TABLES")
 
-for table in my_cursor:
-    print(table[0])
+# for table in my_cursor:
+#     print(table[0])
+
+# Insert in tables data
+# sqlStuff = "INSERT INTO users (name, email, age) VALUES (%s, %s, %s)"
+# record1 = ("Andrei", "apernevan@outlook.com", 38)
+
+# my_cursor.execute(sqlStuff, record1)
+# mydb.commit()
+
+# Inser many records
+sqlStuff = "INSERT INTO users (name, email, age) VALUES (%s, %s, %s)"
+records = [
+    ("Tim", "tim@tim.com", 32),
+    ("Mary", "mary@mary.com", 22),
+    ("Steve", "steve@steveemail.com", 57),
+    ("Tina", "time@something.com", 29),
+]
+
+my_cursor.executemany(sqlStuff, records)
+mydb.commit()
